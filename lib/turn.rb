56aa5a60f62c0bee8097462ba6ff8c69e818c93b
Gabriel Chazanov <gabe.chaz@gmail.com>
2020-10-04 18:36:43 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -10,7 +10,7 @@ def turn(board)
   puts "Please enter 1-9:"
   input = gets.strip
   index = input_to_index(input)
-  if valid_move?(index) == true
+  if valid_move?(board, index) == true
     move(board, index)
   else
     turn(board)
